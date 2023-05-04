c26dec4aa817cce8424e6beb811a992745de7c82
danizavtz <daniellucena@yahoo.com.br>
2023-05-04 07:04:53 -0300

    Initial Commit

---
+++ Shared/dividimosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dividimosApp.swift
+//  Shared
+//
+//  Created by Daniel Lucena Pires on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct dividimosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
