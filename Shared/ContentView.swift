b461c89b0a72929fc4dcd450cc25e9f73dee54c8
danizavtz <daniellucena@yahoo.com.br>
2023-05-04 08:10:42 -0300

    Nova aplicacao

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 0]
+    var totalPerPerson: Double {
+        // calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = Double (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Valor", value: $checkAmount, format: .currency (code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Quantidade de pessoas", selection: $numberOfPeople) {
+                            ForEach(2 ..< 100) {
+                                Text("\($0) pessoas")
+                            }
+                        }
+                }
+                Section {
+                    Picker("Incluir 10% do atendimento", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Deseja incluir 10%?")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("Dividimos")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
